331265018112ba31524195c9f273071d8bfc00de
Tim Ekl <lithium3141@gmail.com>
2012-05-11 10:01:01 -0400

    Improve UI in minor ways
    * Align nodes across top, rather than along left
    * Include jQuery UI stylesheet
    * Present errors reasonably
    
    Signed-off-by: Tim Ekl <lithium3141@gmail.com>

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,15 @@
+function displayError(text) {
+    $("#modal").text(text);
+    $("#modal").dialog({
+        title: "Error",
+        buttons: {"OK": function() { $(this).dialog("close"); }}
+    });
+}
 $(document).ready(function () {
     $.ajax("/api/nodes", {
         dataType: "json",
         error: function(jqxhr, textStatus, errorThrown) {
-            
+            displayError("Failed to fetch list of nodes (" + textStatus + ")");
         },
         success: function(data, textStatus, jqxhr) {
             $.each(data, function(idx, elem) {
