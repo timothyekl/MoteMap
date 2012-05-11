8b494bfec0bdbc9ae8c86e0f99ec951f351bf364
Tim Ekl <lithium3141@gmail.com>
2012-05-11 11:28:02 -0400

    Finish node-coloring implementation
    Signed-off-by: Tim Ekl <lithium3141@gmail.com>

--- public/js/data.js
+++ public/js/data.js
@@ -11,3 +11,11 @@ function adc_to_kelvin(adc) {
 function adc_to_celsius(adc) {
     return adc_to_kelvin(adc) - 273.15;
 }
+
+function zero_pad(number, width) {
+    width -= number.toString().length;
+    if ( width > 0 ) {
+        return new Array( width + (/\./.test( number ) ? 2 : 1) ).join( '0' ) + number;
+    }
+    return number + ""; // always return a string
+}
--- public/js/index.js
+++ public/js/index.js
@@ -37,8 +37,11 @@ function createNodes(idList) {
                 displayError("Failed to get temperature data for node " + elem + " (" + textStatus + ")");
             },
             success: function(data, textStatus, jqxhr) {
-                colorTemperatureFromADC(elem, parseInt(data["temp"]));
-                colorLightFromADC(elem, parseInt(data["light"]));
+                var temp = parseInt(data["temp"]);
+                var light = parseInt(data["light"]);
+                colorTemperatureFromADC(elem, temp);
+                colorLightFromADC(elem, light);
+                setTooltipFromTemperatureLightADC(elem, temp, light);
             }
         });
     });
@@ -46,17 +49,44 @@ function createNodes(idList) {
 
 /**
  * Color the node with the given ID for the temperature represented by the given ADC value.
+ *
+ * Uses the following ranges:
+ *  - < 0 C: HTML blue (#0000ff)
+ *  - 0-25 C: scaled blue, dark to light
+ *  - 25-50 C: scaled red, light to dark
+ *  - > 50 C: HTML red (#ff0000)
  */
 function colorTemperatureFromADC(nodeid, adc) {
     var temp = adc_to_celsius(adc);
-    // TODO this should show temperature by color, not tooltip
+    var tempString;
+    if (temp < 0) {
+        tempString = "#0000ff";
+    } else if (temp < 25) {
+        var color = zero_pad((Math.floor((temp / 25) * 255)).toString(16), 2);
+        tempString = "#" + color + color + "ff";
+    } else if (temp < 50) {
+        var color = zero_pad((Math.floor(((temp - 25) / 25) * 255)).toString(16), 2);
+        tempString = "#ff" + color + color;
+    } else {
+        tempString = "#ff0000";
+    }
+    $(".node#node-" + nodeid + " .temp").css("background-color", tempString);
 }
 
+/**
+ * Color the node with the given ID for the light % represented by the given ADC value.
+ *
+ * Light values are interpreted linearly from 0 to 1023, and shade HTML yellow with black as appropriate.
+ */
 function colorLightFromADC(nodeid, adc) {
     var lightColor = (Math.floor((adc / 1023) * 255)).toString(16);
     $(".node#node-" + nodeid + " .light").css("background-color", "#" + lightColor + lightColor + "00");
 }
 
+/**
+ * Set the node div's title attribute (mouseover tooltip) with the actual values of the light and
+ * temperature data for that node (in degrees Celsius and light %, respectively).
+ */
 function setTooltipFromTemperatureLightADC(nodeid, temp_adc, light_adc) {
     var temp = adc_to_celsius(temp_adc);
     $(".node#node-" + nodeid).attr("title", temp + "C\n" + (light_adc / 1023) * 100 + "% light");
